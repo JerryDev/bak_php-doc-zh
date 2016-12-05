26e6ab7e57fda4485e816e3b26263a03dff546c9
Jerry <JerryDev@163.com>
2016-12-05 10:47:28 +0800

    fix

--- test.php
+++ test.php
@@ -14,24 +14,24 @@ $cfg = array(
 $mysqli = new mysqli($cfg['host'], $cfg['username'], $cfg['password']);
 
 
-if($mysqli -> connect_errno){
-    exit('Connect failed: ' . $mysqli -> connect_error);
+if($mysqli-> connect_errno){
+    exit('Connect failed: ' . $mysqli-> connect_error);
 }
 
-echo 'client_info: ', $mysqli -> client_info, PHP_EOL;
+echo 'client_info: ', $mysqli-> client_info, PHP_EOL;
 echo '-------------', PHP_EOL;
 
-echo 'server_info: ', $mysqli -> server_info, PHP_EOL;
+echo 'server_info: ', $mysqli-> server_info, PHP_EOL;
 echo '-------------', PHP_EOL;
 
-echo 'server_version: ', $mysqli -> server_version, PHP_EOL;
+echo 'server_version: ', $mysqli-> server_version, PHP_EOL;
 echo '-------------', PHP_EOL;
 
-echo 'client_version: ', $mysqli -> client_version, PHP_EOL;
+echo 'client_version: ', $mysqli-> client_version, PHP_EOL;
 echo '-------------', PHP_EOL;
 
 
-echo 'protocol_version: ', $mysqli -> protocol_version, PHP_EOL;
+echo 'protocol_version: ', $mysqli-> protocol_version, PHP_EOL;
 echo '-------------', PHP_EOL;
 
 
